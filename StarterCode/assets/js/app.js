c862cec36f7edf98e17bca3879e5c64e21a42e0d
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-07-24 10:09:42 -0500

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,11 @@
 // @TODO: YOUR CODE HERE!
+
+// 1.- Chart Params
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 60, left: 50 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
